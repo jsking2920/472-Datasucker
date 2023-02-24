81a6b3a1d8cf53a96a3aabce66621de11f07906a
Scott King <jsking@andrew.cmu.edu>
2023-02-24 15:01:55 -0500

    Add bg music and sfx

--- Datasucker/Assets/Scripts/InteractableComponent.cs
+++ Datasucker/Assets/Scripts/InteractableComponent.cs
@@ -5,23 +5,17 @@ using UnityEngine;
 
 public class InteractableComponent : MonoBehaviour
 {
-
     public float InteractionRange = 6.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _onTapClip;
 
     public virtual void OnObjectTapped()
     {
         Debug.Log(name + " tapped.");
+        if (_audioSource && _onTapClip)
+        {
+            _audioSource.PlayOneShot(_onTapClip);
+        }
     }
 }
