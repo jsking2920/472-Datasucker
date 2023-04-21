8496ee98636045a18c75f1236d59ab539745a75f
lelayuan719 <101837319+lelayuan719@users.noreply.github.com>
2023-04-21 17:31:52 -0400

    UI menu

---
+++ Datasucker/Assets/Scripts/UI script/LevelManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager S;
+
+    public string levelName;
+
+    public string nextSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        S = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void btn_StartTheGame()
+    {
+        Debug.Log("StartPushed");
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void btn_BackToMain()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void btn_Tutorial()
+    {
+        SceneManager.LoadScene("NewTutorial");
+    }
+
+    public void btn_Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+}
